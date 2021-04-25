2a6663d87aaea64461d97c3861ca85fe7ee793f7
Cullen Pu <Cullenpu@gmail.com>
2021-04-25 18:54:47 -0400

    Serve using default node server instead of express

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-"use strict";
-
-const express = require("express");
-const app = express();
-
+const http = require("http");
+const fs = require("fs");
 const path = require("path");
 
-// Set up static directory for files in /pub using Express middleware
-app.use(express.static(path.join(__dirname, "/pub")));
+const server = http.createServer((req, res) => {
+  const pub = path.join(__dirname, "/pub");
+  const filepath =
+    req.url === "/" ? path.join(pub, "index.html") : path.join(pub, req.url);
 
-// Use environment variable process.env.PORT for deployment
+  const extname = String(path.extname(filepath)).toLowerCase();
+  const mimeTypes = {
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".css": "text/css",
+    ".jpg": "image/jpg",
+    ".svg": "image/svg+xml",
+  };
+  const contentType = mimeTypes[extname] || "application/octet-stream";
+
+  fs.readFile(filepath, function (err, data) {
+    if (err) {
+      fs.readFile(path.join(pub, "index.html"), function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data, "utf-8");
+      });
+    } else {
+      res.writeHead(200, { "Content-type": contentType });
+      res.end(data, "utf-8");
+    }
+  });
+});
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
